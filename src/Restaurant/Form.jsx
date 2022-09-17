e4a45b439fc822248c7205b898db8aa13b9999f7
aayushshuk1a <aayush5emails@gmail.com>
2022-09-18 05:09:31 +0530

    Restaurant

---
+++ src/Restaurant/Form.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./form.scss";
+
+const Form = () => {
+  return (
+    <div className='Card'>
+        <h1>Food Details</h1>
+        <form>
+            
+            <label>Name</label><br />
+           <input type="text"  id='name'/><br />  
+        
+           
+           
+            <label>Name</label><br />
+           <input type="text"  id='name'/><br />  
+            
+            <label>Name</label><br />
+           <input type="text"  id='name'/><br />  
+            
+           
+            <label>Name</label><br />
+           <input type="text"  id='name'/><br />  
+            
+        
+
+        </form>
+    </div>
+  )
+}
+
+export default Form
